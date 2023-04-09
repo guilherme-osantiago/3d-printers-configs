f2f1a41c8ca0c1bd0c0c91a760128ac03601cb3f
guilherme-osantiago <gob.santiago@gmail.aqui>
2023-04-09 17:27:01 -0300

    Setup to use Z_STEPPER_AUTO_ALIGN

--- Creuza/Marlin-2.0.9.5/Marlin/Configuration_adv.h
+++ Creuza/Marlin-2.0.9.5/Marlin/Configuration_adv.h
@@ -923,7 +923,7 @@
  * Z Steppers Auto-Alignment
  * Add the G34 command to align multiple Z steppers using a bed probe.
  */
-//#define Z_STEPPER_AUTO_ALIGN
+#define Z_STEPPER_AUTO_ALIGN
 #if ENABLED(Z_STEPPER_AUTO_ALIGN)
   /**
    * Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
@@ -931,7 +931,7 @@
    * If not defined, probe limits will be used.
    * Override with 'M422 S<index> X<pos> Y<pos>'.
    */
-  //#define Z_STEPPER_ALIGN_XY { {  10, 190 }, { 100,  10 }, { 190, 190 } }
+  #define Z_STEPPER_ALIGN_XY { {  10, 190 }, { 100,  10 } }
 
   /**
    * Orientation for the automatically-calculated probe positions.
@@ -953,6 +953,7 @@
    *               | 1   2 | 2   3 | 3   4 | 4   1 |
    */
   #ifndef Z_STEPPER_ALIGN_XY
+  // TODO: Se piorar ao invés de melhorar, utilize o recurso abaixo
     //#define Z_STEPPERS_ORIENTATION 0
   #endif
 
@@ -974,7 +975,7 @@
 
   // On a 300mm bed a 5% grade would give a misalignment of ~1.5cm
   #define G34_MAX_GRADE              5    // (%) Maximum incline that G34 will handle
-  #define Z_STEPPER_ALIGN_ITERATIONS 3    // Number of iterations to apply during alignment
+  #define Z_STEPPER_ALIGN_ITERATIONS 5    // Number of iterations to apply during alignment
   #define Z_STEPPER_ALIGN_ACC        0.02 // Stop iterating early if the accuracy is better than this
   #define RESTORE_LEVELING_AFTER_G34      // Restore leveling after G34 is done?
   // After G34, re-home Z (G28 Z) or just calculate it from the last probe heights?
